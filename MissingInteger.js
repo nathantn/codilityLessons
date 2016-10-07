20bbeb7a1c8689fdd1dcc1bd8bcdb18bc72b9509
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2016-10-07 16:37:10 -0300

    Missing integer solution

---
+++ MissingInteger.js
@@ -0,0 +1,16 @@
+function solution(A) {
+    let minimalPositiveInteger = 1;
+
+    A = A.sort((a, b) => {
+        if (a > b) return 1;
+        if (a < b) return -1;
+        return 0;
+    });
+    
+    for (let i = 0, max = A.length; i < max; i += 1) {      
+        let num = A[i];   
+        if (num > 0 && num === minimalPositiveInteger) minimalPositiveInteger = num + 1;
+    }
+    
+    return minimalPositiveInteger;
+}
